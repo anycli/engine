807dc0577d42ecd38316f11d9577e35a388e5de1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-25 02:47:12 -0800

    fix: updated deps

--- test/engine.test.ts
+++ test/engine.test.ts
@@ -1,32 +1,6 @@
-import {describe, expect, it, output} from '@dxcli/dev-test'
+import {expect, fancy} from 'fancy-mocha'
 
 import {run} from '../src'
-
-export interface RunCommandOptions {
-  description?: string
-  stdout?: string
-  stderr?: string
-  exit?: number
-}
-const itRunsCommand = (args: string[], opts: RunCommandOptions) => {
-  const description = opts.description || args[0]
-  let test = it
-  if (opts.stdout) test = test.stdout
-  if (opts.stderr) test = test.stderr
-  test(description, async () => {
-    const exit = opts.exit || 0
-    try {
-      await run(args)
-    } catch (err) {
-      if (err.code !== 'EEXIT') throw err
-      if (err['cli-ux'].exit !== exit) {
-        throw new Error(`Expected exit code to be ${exit} but got ${err['cli-ux'].exit}`)
-      }
-    }
-    if (opts.stdout) expect(output.stdout).to.equal(opts.stdout)
-    if (opts.stderr) expect(output.stderr).to.equal(opts.stderr)
-  })
-}
 
 const pjson = require('../package.json')
 
@@ -34,8 +8,24 @@ describe('CLI', () => {
   describe('version', () => {
     const stdout = `@dxcli/engine/${pjson.version} (${process.platform}-${process.arch}) node-${process.version}\n`
 
-    itRunsCommand(['--version'], {stdout})
-    itRunsCommand(['-v'], {stdout})
-    itRunsCommand(['version'], {stdout})
+    fancy()
+    .stdout()
+    .run(() => run(['--version']))
+    .catch((err: any) => expect(err['cli-ux'].exit).to.equal(0))
+    .run(output => expect(output.stdout).to.equal(stdout))
+    .end('--version')
+
+    fancy()
+    .stdout()
+    .run(() => run(['-v']))
+    .catch((err: any) => expect(err['cli-ux'].exit).to.equal(0))
+    .run(output => expect(output.stdout).to.equal(stdout))
+    .end('-v')
+
+    fancy()
+    .stdout()
+    .run(() => run(['version']))
+    .run(output => expect(output.stdout).to.equal(stdout))
+    .end('version')
   })
 })
