b9e390228a2ecd0ae145876ea0ad1cabc4b6573d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-30 04:35:38 -0800

    fix: added typescript tests

---
+++ test/typescript.test.ts
@@ -0,0 +1,42 @@
+import {IEngine, read} from '@dxcli/config'
+import {expect, fancy} from 'fancy-test'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+
+import {Engine} from '../src'
+
+const loadPlugin = (root: string) => async (ctx: {engine: IEngine}) => {
+  await reset(root)
+  const config = await read({root})
+  ctx.engine = new Engine()
+  await ctx.engine.load(config)
+}
+
+const reset = async (root: string) => {
+  await fs.outputFile(path.join(root, '.git'), '')
+  const config = await read({root})
+  await fs.remove(config.cacheDir)
+  await fs.remove(config.dataDir)
+}
+
+describe('hooks', () => {
+  fancy
+  .do(loadPlugin(path.join(__dirname, 'fixtures/typescript')))
+  .stdout()
+  .do(async ctx => {
+    const cmd = await ctx.engine.commands[0].load()
+    await cmd.run([])
+    expect(ctx.stdout).to.equal('it works!\n')
+  })
+  .it('loads a TS plugin')
+
+  fancy
+  .do(loadPlugin(path.join(__dirname, 'fixtures/typescript2')))
+  .stdout()
+  .do(async ctx => {
+    const cmd = await ctx.engine.commands[0].load()
+    await cmd.run([])
+    expect(ctx.stdout).to.equal('it works 2!\n')
+  })
+  .it('loads 2 TS plugins')
+})
