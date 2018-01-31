cf7c3d78536d90687cd768c9d1179d84930aa53e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-31 13:33:25 -0800

    fix: fixed run command

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as Config from '@anycli/config'
 import Command from './command'
 import Engine from './engine'
 
-export function run(argv = process.argv.slice(2), opts: Config.IConfig | Config.ICommandOptions = {}) {
+export function run(argv = process.argv.slice(2), opts: Config.ICommandOptions = {}) {
   return Command.run(argv, opts)
 }
 
