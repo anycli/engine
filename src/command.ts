b9d64d103ef3c8f317d0e3f556b16781291cc432
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-02 03:32:13 -0800

    fix: fixed --help

--- src/command.ts
+++ src/command.ts
@@ -1,13 +1,18 @@
 import CommandBase from '@anycli/command'
+import Help from '@anycli/plugin-help'
+import cli from 'cli-ux'
 
 import Engine from './engine'
 
 export default class Command extends CommandBase {
+  static type = 'engine'
+
   engine = new Engine()
 
   async run() {
     await this.engine.load(this.config)
     let id = this.argv[0]
+    if (id === '-h' || this.argv.includes('--help')) return this.showHelp()
     if (!id) id = 'help'
     await this.engine.runHook('init', {id})
     const cachedCommand = this.engine.findCommand(id)
@@ -21,5 +26,19 @@ export default class Command extends CommandBase {
   protected async commandNotFound(id: string) {
     await this.engine.runHook('command_not_found', {id})
     throw new Error(`command not found: ${id}`)
+  }
+
+  protected showHelp() {
+    let id = this.argv.find(a => !a.startsWith('-'))
+    const HHelp: typeof Help = require('@anycli/plugin-help').default
+    let help = new HHelp(this.config)
+    if (!id) {
+      let rootHelp = help.root()
+      cli.info(rootHelp)
+    } else {
+      let command = this.engine.findCommand(id, true)
+      let commandHelp = help.command(command)
+      cli.info(commandHelp)
+    }
   }
 }
