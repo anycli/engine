f19aa9cea753460605790aef3567d6326437ea2d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-01 22:00:39 -0800

    feat: added node version check

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import * as path from 'path'
+import * as semver from 'semver'
+
+function checkNodeVersion() {
+  try {
+    const root = path.join((module.parent && module.parent.filename) || __filename, '../..')
+    const pjson = require(path.join(root, 'package.json'))
+    const nodeVersion = process.versions.node
+    const engine = pjson.engines && pjson.engines.node
+    if (!engine) return
+    if (!semver.satisfies(nodeVersion, engine)) {
+      process.stderr.write(`WARNING\nWARNING Node version must be ${pjson.engines.node} to use ${pjson.name}\nWARNING\n`)
+    }
+  } catch {}
+}
+checkNodeVersion()
+
 import * as Config from '@anycli/config'
 
 import Command from './command'
